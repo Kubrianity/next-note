e6a300907b05a29f7762f7080b516ae12d70124d
Kubrianity <kubraaozerenn@gmail.com>
2024-01-24 23:43:31 +0300

    Fetch all notes, iterate over them to display components

--- app/page.jsx
+++ app/page.jsx
@@ -1,10 +1,24 @@
 import NoteCard from './(components)/NoteCard';
 
-const Dashboard = () => {
+const getNotes = async() => {
+  try {
+    const res = await fetch("http://localhost:3000/api/Notes", {
+      cache: "no-store"
+    })
+    return res.json();
+  }
+  catch(err) {
+    console.log("Failed to get notes", err)
+  }
+}
+const Dashboard = async () => {
+  const {notes} = await getNotes()
 	return (
 		<main className="p-5">
 			<div className="lg:grid grid-cols-2 xl:grid-cols-4">
-				<NoteCard />
+        {notes.map((note, index) => (
+          <NoteCard key={index} id={index} note={note} />)
+        )}
 			</div>
 		</main>
 	);
