2d12a3ae7e93e877ca5f5f1ba3c67a753617fe44
Kubrianity <kubraaozerenn@gmail.com>
2024-01-25 12:29:19 +0300

    Add props

--- app/(components)/NoteCard.jsx
+++ app/(components)/NoteCard.jsx
@@ -2,23 +2,23 @@ import DeleteIcon from './DeleteIcon';
 import PriorityDisplay from './PriorityDisplay';
 import StatusDisplay from './StatusDisplay';
 
-const NoteCard = () => {
+const NoteCard = ({note}) => {
 	return (
 		<div>
 			<div className="flex flex-col hover:bg-card-hover bg-card rounded-md shadow-lg p-3 m-2">
 				<div className="flex mb-3">
-					<PriorityDisplay />
+					<PriorityDisplay priority={note.priority} />
 					<div className="ml-auto">
 						<DeleteIcon />
 					</div>
 				</div>
-        <h4>Note Title</h4>
+        <h4>{note.title}</h4>
         <hr></hr>
-        <p>This is a note description</p>
+        <p>{note.description}</p>
 				<div className="flex-grow"></div>
 				<div className="flex mt-2">
 					<div className="ml-auto  flex items-end">
-						<StatusDisplay />
+						<StatusDisplay status={note.status} />
 					</div>
 				</div>
 			</div>
--- app/(components)/StatusDisplay.jsx
+++ app/(components)/StatusDisplay.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 
-function StatusDisplay() {
+function StatusDisplay({status}) {
 	return (
 		<span className="inline-block rounded-full px-2 py-1 text-xs font-semibold text-slate-700 bg-button">
-			Uncompleted
+			{status}
 		</span>
 	);
 }
