3d04a3ed479887d2bdb732160fdb924a32471cc2
Kubrianity <kubraaozerenn@gmail.com>
2024-01-26 20:04:52 +0300

    Change icon color by priority

--- app/(components)/PriorityDisplay.jsx
+++ app/(components)/PriorityDisplay.jsx
@@ -1,12 +1,12 @@
 import { faStar } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
-const PriorityDisplay = () => {
+const PriorityDisplay = ({priority}) => {
 	return (
 		<div className="flex justify-start align-baseline text-slate-700">
-			<FontAwesomeIcon icon={faStar} />
-			<FontAwesomeIcon icon={faStar} />
-			<FontAwesomeIcon icon={faStar} />
+			<FontAwesomeIcon icon={faStar} className={`${priority > 0 ? "text-slate-700" : "text-default-text"}`} />
+			<FontAwesomeIcon icon={faStar} className={`${priority > 1 ? "text-slate-700" : "text-default-text"}`} />
+			<FontAwesomeIcon icon={faStar} className={`${priority > 2 ? "text-slate-700" : "text-default-text"}`} />
 		</div>
 	);
 };
