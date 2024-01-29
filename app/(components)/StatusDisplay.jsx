a8b08f92df7fe32404af4619ae212012b5cfe355
Kubrianity <kubraaozerenn@gmail.com>
2024-01-29 21:30:54 +0300

    Change background color by status

--- app/(components)/StatusDisplay.jsx
+++ app/(components)/StatusDisplay.jsx
@@ -1,8 +1,18 @@
-import React from 'react';
-
-function StatusDisplay({status}) {
-	return (
-		<span className="inline-block rounded-full px-2 py-1 text-xs font-semibold text-slate-700 bg-button">
+const StatusDisplay = ({status}) => {
+  const statusColor = (status) => {
+    let color = ''
+    switch(status.toLowerCase()) {
+      case "completed":
+        color = "bg-green-400"
+        return color;
+      case "uncompleted":
+        color = "bg-button" 
+        return color;
+    }
+    return color;
+  }
+  return (
+		<span className={`inline-block rounded-full px-2 py-1 text-xs font-semibold text-slate-700 ${statusColor(status)}`}>
 			{status}
 		</span>
 	);
