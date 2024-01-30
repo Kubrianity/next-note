f20972c367edaa0beb9b7a07f5d4b9fa68d4e256
Kubrianity <kubraaozerenn@gmail.com>
2024-01-30 12:25:32 +0300

    Update priority component

--- app/(components)/PriorityDisplay.jsx
+++ app/(components)/PriorityDisplay.jsx
@@ -4,6 +4,7 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 const PriorityDisplay = ({priority}) => {
 	return (
 		<div className="flex justify-start align-baseline text-slate-700">
+      <p className="font-medium">Priority: </p>
 			<FontAwesomeIcon icon={faStar} className={`${priority > 0 ? "text-slate-700" : "text-default-text"}`} />
 			<FontAwesomeIcon icon={faStar} className={`${priority > 1 ? "text-slate-700" : "text-default-text"}`} />
 			<FontAwesomeIcon icon={faStar} className={`${priority > 2 ? "text-slate-700" : "text-default-text"}`} />
