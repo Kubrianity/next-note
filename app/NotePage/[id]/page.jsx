97550936ec48848300297ab74167e0709f6f12f1
Kubrianity <kubraaozerenn@gmail.com>
2024-02-06 15:03:53 +0300

    Populate note attr with fetched data

--- app/NotePage/[id]/page.jsx
+++ app/NotePage/[id]/page.jsx
@@ -1,8 +1,33 @@
 import NoteForm from "../../(components)/NoteForm"
 
-const NotePage = () => {
+const getNote = async (id) => {
+  try {
+    const res = await fetch(`http://localhost:3000/api/Notes/${id}`, {
+      method: 'GET',
+      cache: "no-store",
+    });
+
+    if (!res.ok) {
+      throw new Error("Failed to fetch note");
+    }
+    return res.json();
+  } 
+  catch (error) {
+    console.log(error);
+  }
+}
+
+const NotePage = async ({ params }) => {
+
+  const isEditing = params.id != "new" ? true: false
+
+  if(isEditing) {
+    const res = await getNote(params.id)
+    var note = res.data
+  }
+
   return (
-    <NoteForm />
+    <NoteForm note={note} />
   )
 }
 
